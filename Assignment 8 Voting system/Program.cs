ee62e025ae84068e9dd9440e259ed34d3f298862
Cattress <priyaphon.vincent@gmail.com>
2021-01-26 15:13:28 +0700

    Add project files.

---
+++ Assignment 8 Voting system/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Assignment_8_Voting_system
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Vote = new int[4];
+            Vote[0] = 1;
+            Vote[1] = 1;
+            Vote[2] = 3;
+            Vote[3] = 1;
+            Vote[4] = 2;
+               
+        }
+
+        static void CastVotes(Vote[] votes)
+        {
+            while ( UserInput <> 0)
+            {
+
+                Console.WriteLine("What is your name?");
+                voterName[i] = Console.ReadLine();
+
+                Console.WriteLine("Insert Vote");
+                UserInput = Console.Read();
+
+                if UserInput = 1; 
+                {
+
+                }
+
+
+                i++;
+            }
+            
+        }
+
+    }
+}
---
+++ Assignment 8 Voting system/Votes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_8_Voting_system
+{
+    class Votes
+    {
+        private string partyName;
+        private DateTime TimeVoted;
+        private string voterName;
+        private static int totalVotes;
+        private static int UserInput;
+
+
+        private static Boolean ContinueVote;
+
+
+        public Votes(string partyName, string voterName)
+        {
+            this.partyName = partyName;
+            this.voterName = voterName;
+            TimeVoted = DateTime.Now;
+            totalVotes++;
+        }
+        public string GetPartyName()
+        {
+            return partyName;
+        }
+
+        public int GetTotalVotes()
+        {
+            return totalVotes;
+        }
+
+
+    }
+
+}
